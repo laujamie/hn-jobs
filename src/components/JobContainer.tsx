9c9a646c397925d4e4ca159c9796398071c9671c
laujamie <36782341+laujamie@users.noreply.github.com>
2023-04-11 13:40:55 -0400

    Clean up code and add virtualization

--- src/components/JobContainer.tsx
+++ src/components/JobContainer.tsx
@@ -1,6 +1,9 @@
-import { ReactNode, useState } from "react";
+import { useState } from "react";
+import type { ReactNode } from "react";
 import dayjs from "dayjs";
 import Fuse from "fuse.js";
+import { Virtuoso } from "react-virtuoso";
+import { sanitize } from "isomorphic-dompurify";
 import type { ItemV0 } from "@lib/hn";
 
 export interface JobContainerProps {
@@ -18,7 +21,6 @@ export default function JobContainer({ items }: JobContainerProps): ReactNode {
     }
     const fuse = new Fuse(items, {
       keys: ["text"],
-      includeScore: true,
     });
     const result = fuse.search(query);
     if (result.length > 0) {
@@ -30,34 +32,36 @@ export default function JobContainer({ items }: JobContainerProps): ReactNode {
   };
 
   return (
-    <div className="overflow-hidden space-y-4">
+    <div className="space-y-4 h-full">
       <input
         type="search"
         onChange={(e) => searchItem(e.target.value)}
         placeholder="Search for job postings"
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       />
-      {displayItems != null &&
-        displayItems
-          .sort((a, b) => (a.time > b.time ? -1 : 0))
-          .map((item) =>
-            item.text != null ? (
+      {displayItems != null && (
+        <Virtuoso
+          className="h-full"
+          data={displayItems.sort((a, b) => (a.time >= b.time ? -1 : 0))}
+          useWindowScroll
+          itemContent={(_, item) => (
+            <div
+              className="block p-6 rounded-lg border-gray-200 border break-words mb-2"
+              key={item.id}
+            >
+              <h2 className="text-gray-800 text-3xl font-semibold">
+                {dayjs.unix(item.time).format("MMM D, YYYY, h:mm a")}
+              </h2>
               <div
-                className="block p-6 rounded-lg border-gray-200 border"
-                key={item.id}
-              >
-                <h2 className="text-gray-800 text-3xl font-semibold">
-                  {dayjs.unix(item.time).format("MMM D, YYYY, h:mm a")}
-                </h2>
-                <div
-                  className="mt-2 text-gray-600"
-                  dangerouslySetInnerHTML={{
-                    __html: item.text,
-                  }}
-                />
-              </div>
-            ) : null
+                className="mt-2 text-gray-600"
+                dangerouslySetInnerHTML={{
+                  __html: sanitize(item.text ?? ""),
+                }}
+              />
+            </div>
           )}
+        />
+      )}
     </div>
   );
 }
